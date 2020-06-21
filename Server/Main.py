7e88c32481f333931fdff0ac198792bc19f1d87e
Matthew Hao <49629333+NothingH@users.noreply.github.com>
2020-06-21 15:02:59 +0800

    Add files via upload

--- Server/Main.py
+++ Server/Main.py
@@ -113,8 +113,24 @@ serverport=int(conf['Port'])
 # *************************************************
 
 client_socket=[]    #record the opening socket
+##########################UDP Delay_Response##########################################
+HOST = '0.0.0.0'
+PORT = serverport
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+udpSerSock = socket(AF_INET, SOCK_DGRAM)
+udpSerSock.bind(ADDR)
+while True:
+    print('waiting for message...')
+    data, addr = udpSerSock.recvfrom(BUFSIZE)
+    rpldata=bytes('ACCEPT [%s] %s' % (time.ctime(), data),encoding='utf-8')
+    udpSerSock.sendto(rpldata, addr)
+    print('received from %s >> %s' % (addr, data))
+udpSerSock.close()
 
-############################MAIN WORK####################################
+
+############################Connection Contribute####################################
+
 #receive message from client 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverport))
