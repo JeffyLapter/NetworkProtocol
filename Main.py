55730665c95785f32f6ca1b1eea36617758625cc
Matthew Hao <49629333+NothingH@users.noreply.github.com>
2020-06-22 01:21:44 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,14 +4,13 @@ serverPort = 8803
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
-t=time.clock()
+
 print('The server is ready to receive')
 connectionSocket, addr = serverSocket.accept()
 while True:
      sentence = connectionSocket.recv(1024).decode()
      capitalizedSentence = sentence.upper()
      connectionSocket.send(capitalizedSentence.encode())
-     if time.time()-t>=300:
-         break
+ 
 connectionSocket.close()
                     
